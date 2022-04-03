64fe7aaa0f48feadf94a6ef52bd535dbd4106f03
Loic Jacquel <loic.jacquel@gmail.com>
2022-04-03 21:24:05 +0900

    Revert "update write mode"
    This reverts commit 2d321d97f372e68961b1c683d5866f46de03f6fc.

--- src/pages/Pronostics.js
+++ src/pages/Pronostics.js
@@ -22,7 +22,7 @@ export default function Pronostics() {
   return (
     <Container>
       <Button className="my-2" variant="success" onClick={handlePronosticClick}>
-        Submita
+        Submit
       </Button>
       <Row>
         {categories.map((category, index) => (
