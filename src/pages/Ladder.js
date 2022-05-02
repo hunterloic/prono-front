3b0634595ea6ba792982ebff46f617065c639eb1
Loic Jacquel <loic.jacquel@gmail.com>
2022-05-02 15:50:32 +0900

    add group ladder management

--- src/pages/Ladder.js
+++ src/pages/Ladder.js
@@ -6,7 +6,7 @@ export default function Ladder() {
   const { axios } = useAxios();
   const [ladders, setLadders] = useState([]);
   const [groupId, setGroupId] = useState("");
-  const [error, setError] = useState("");
+  const [info, setInfo] = useState(null);
 
   useEffect(() => {
     async function fetchLadders() {
@@ -18,27 +18,32 @@ export default function Ladder() {
 
   const handleJoinGroup = async () => {
     if (groupId === "") {
-      setError("Group id is mandatory");
+      setInfo("Group id is mandatory");
     }
-    await axios.get(`/group/join?password=${groupId}`).catch((err) => {
-      if (err.response.status == 404) {
-        setError("Group does not exists");
-      }
-    });
+
+    const response = await axios
+      .get(`/group/join?password=${groupId}`)
+      .catch((err) => {
+        if (err.response.status == 404) {
+          setInfo({ title: "Error", message: "Group does not exists" });
+        }
+      });
+
+    if (response.status === 204) {
+      setInfo({ title: "Info", message: "Join group successfully" });
+    }
   };
 
-  const handleClose = () => setError("");
-
-  console.log(ladders);
+  const handleClose = () => setInfo("");
 
   return (
-    <Container>
-      {error && (
-        <Modal show={error !== ""} onHide={handleClose}>
+    <Container className="mx-10">
+      {info && (
+        <Modal show={info} onHide={handleClose}>
           <Modal.Header closeButton>
-            <Modal.Title>Error</Modal.Title>
+            <Modal.Title>{info.title}</Modal.Title>
           </Modal.Header>
-          <Modal.Body>{error}</Modal.Body>
+          <Modal.Body>{info.message}</Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose}>
               Close
@@ -46,24 +51,28 @@ export default function Ladder() {
           </Modal.Footer>
         </Modal>
       )}
-      <div>
-        <Alert variant="info" className="mx-1 my-2 p-2">
-          You are not in any group. You can join a group to see the ladder.
-        </Alert>
+      <div className="mx-1 my-2 p-2">
+        {ladders.length <= 0 && (
+          <div>
+            <Alert variant="info">
+              You are not in any group. You can join a group to see the ladder.
+            </Alert>
+          </div>
+        )}
+        <Stack direction="horizontal" gap={2}>
+          <div>Enter the group identifier to join a group :</div>
+          <div>
+            <Form.Control
+              placeholder="group id"
+              value={groupId}
+              onChange={(e) => setGroupId(e.target.value)}
+            />
+          </div>
+          <div>
+            <Button onClick={handleJoinGroup}>Join</Button>
+          </div>
+        </Stack>
       </div>
-      <Stack direction="horizontal" gap={2}>
-        <div>Enter the group identifier to join a group :</div>
-        <div>
-          <Form.Control
-            placeholder="group id"
-            value={groupId}
-            onChange={(e) => setGroupId(e.target.value)}
-          />
-        </div>
-        <div>
-          <Button onClick={handleJoinGroup}>Join</Button>
-        </div>
-      </Stack>
     </Container>
   );
 }
